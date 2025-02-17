fe281e5963ec57bd7b316bdc6be2eac9dc144450
mitaamara <mitaamara@gmail.com>
2025-02-17 11:41:08 +0700

    skip verify authentication token before action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    skip_before_action :verify_authenticity_token  
     before_action :set_task, only: [:show, :update, :destroy]
 
     # Get all task
